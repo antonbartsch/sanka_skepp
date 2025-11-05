d93f723bb3551ada6a470d38bf501e117f96a4a8
antonbartsch <anton.bartsch@outlook.com>
2025-11-05 17:41:09 +0100

    lade till en board klass och metoderna generate_board och analyze_hits

--- board.py
+++ board.py
@@ -27,3 +27,101 @@ class Square:
             return "O" #träff på tomm ruta
         else:
             return " " #obeskjuten tom ruta
+        
+class Board:
+    """ representerar spelplanen
+
+    Atribut:
+    squares (list): 2D-lista av Square-objekt som representerar spelplanen
+    """
+    def __init__ (self):
+        """ initiera spelplanen"""
+        self.squares = []
+
+    def generate_board (self, HEIGHT, WIDTH):
+        """ generera en 2D-lista av Square-objekt baserat på angivna dimensioner 
+        
+        Argument:
+            HEIGHT (int): höjden på spelplanen
+            WIDTH (int): bredden på spelplanen
+        """
+        for row in range (HEIGHT):
+            row = []
+            for square in range (WIDTH):
+                square = Square()
+                row.append(square)
+            self.squares.append(row)
+
+    def analyze_hits (self):
+        """
+        analysera träffar på spelplanen och returnera träffprocent och om speler är vunnet
+
+        Returnerar:
+            hit_percentage (float): procentandel träffar på skepp
+            victory (bool): True om alla skepp är träffade annars False
+        """
+        total_ship_squares = 0
+        total_hit_ship_squares = 0
+        total_hit_squares = 0
+
+        for row in self.squares:
+            for square in row:
+                if square.ship:
+                    total_ship_squares += 1
+                    if square.hit:
+                        total_hit_ship_squares += 1
+                if square.hit:
+                    total_hit_squares += 1
+        
+        if total_ship_squares == 0:
+            hit_percentage = 0.0
+        else:
+            hit_percentage = total_hit_ship_squares / total_ship_squares*100
+        if total_hit_ship_squares == total_ship_squares
+            victory = True
+        else:
+            victory = False
+        return hit_percentage, victory
+
+    def __str__(self):
+        """ returnera en strängrepresentation av spelplanen 
+        
+        Returnerar:
+            board_str (str): strängrepresentation av spelplanen
+        """
+        board_str = ""
+        for row in self.squares:
+            for square in row:
+                board_str += str(square)
+            board_str += "\n"
+        return board_str
+    
+    def test_square_values(self):
+        """ testfunktion för att skriva ut olika attributvärden för varje ruta på spelplanen """
+        for row in self.squares:
+            for square in row:
+                square.ship=True
+                square.hit=True
+                square.hidden=False
+        print(self)
+        for row in self.squares:
+            for square in row:
+                square.ship=False
+                square.hit=True
+                square.hidden=False
+        print(self)
+        for row in self.squares:
+            for square in row:
+                square.ship=True
+                square.hit=False
+                square.hidden=False
+        print(self)
+def test():
+    """ testfunktion för att skapa och skriva ut en spelplan """
+    board = Board()
+    board.generate_board(5, 5)
+    print(board)
+    board.test_square_values()
+
+if __name__ == "__main__":
+    test()
