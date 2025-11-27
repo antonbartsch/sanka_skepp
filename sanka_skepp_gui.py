e379587db9bf7a618d945d03f0a64f3eee8c7542
antonbartsch <anton.bartsch@outlook.com>
2025-11-27 16:19:11 +0100

    lade till separat frame för meny

--- sanka_skepp_gui.py
+++ sanka_skepp_gui.py
@@ -4,8 +4,47 @@ from tkinter import messagebox
 from game_functions import *
 from board import *
 
+class start_menu(Frame):
+    """Hantera gui versionen av startmenyn"""
+
+    def __init__(self, master=None):
+        """Initierar ett start_menu objekt"""
+        Frame.__init__(self, master)
+        self.pack()
+        self.generate_menu()
+
+    def generate_menu(self):
+        """skapa startmenyn till gui spelet"""
+        self.title = Label(self, text="Välkommen till Sänka Skepp")
+        self.title.pack()
+
+        self.new_game_button = create_button("Starta nytt spel", self.start_new_game)
+        self.new_game_button.pack()
+
+        self.top_list_button = create_button("Topplista", self.show_top_list)
+        self.top_list_button.pack()
+
+        self.rules_button = create_button("Regler", self.show_rules)
+        self.rules_button.pack()
+
+        self.quit_button = create_button("Avsluta", self.quit_game)
+        self.quit_button.pack()
+
+    def start_new_game(self):
+        
+        gui_board = Gui_board(master=self.master)
+        gui_board.create_board(6,7)
+        gui_board.generate_ship(5)
+    def show_top_list(self):
+        top_list = read_top_list()
+        messagebox.showinfo("Toplista", format_top_list(top_list))
+    def show_rules(self):
+        pass  #todo: implementera visa regler funktionalitet
+    def quit_game(self):
+        self.master.destroy()
+
 class Gui_board(Frame,Board):
-    """Hanterar gui versionen av spelbrädets utseende och funktionalitet
+    """Hantera gui versionen av spelbrädets utseende och funktionalitet
     Ärver från Frame och Board klasserna
     """
     def __init__(self, master=None):
@@ -43,7 +82,10 @@ class Gui_board(Frame,Board):
 
     def update_board(self):
         """Uppdaterar alla knappar på spelbrädet"""
-        self.analyze_hits()
+        hit_percentage, victury = self.analyze_hits()
+        if victury:
+            messagebox.showinfo("Grattis!", "Du har sänkt alla skepp!")
+            #todo: victory
         for row in self.squares:
             for square in row:
                 square.update_button()
@@ -86,17 +128,43 @@ class Button_square(Button, Square):
             self.config(bg='darkblue')  #träff på tomm ruta
         else:
             self.config(bg='lightblue') #obeskjuten tom ruta
+
+def format_top_list(top_list):
+    """Formatera topplistan så att den kan skrivas ut i en messagebox
+    
+    Argument:
+        toplist (List): lista med toppresultat
+        
+    Returnerar:
+        formatted_list (str): formaterad sträng med topplistan
+    """
+    formated_list = "Topplista:\n"
+    for player_score in top_list:
+        formated_list += f"{player_score[1]}: {player_score[0]}%\n"
+    return formated_list
             
+def create_button(text, command):
+    """Skapa knappar snabbt
+
+    Argument:
+        text (str): texten på knappen
+        command (function): funktion som körs när knappen trycks
+    Returnerar:
+        button (Button): den skapade knappen
+    """
+    button = Button(text=text, command=command)
+    return button
 
 def main():
     """Huvudfunktion för gui applikationen"""
     root = Tk()
-    gui_board = Gui_board(root)
-    gui_board.create_board(6,7)
-    gui_board.generate_ship(5)
-    gui_board.generate_ship(3)
-    gui_board.generate_ship(2)
-    gui_board.hide_ships()
+    #board = Gui_board(master=root)
+    #root.board = board
+    #board.create_board(6,7)
+    #board.generate_ship(5)
+    #board.generate_ship(3)
+    #board.generate_ship(2)
+    root.app = start_menu(master=root)
     root.mainloop()
 
 if __name__ == "__main__":
